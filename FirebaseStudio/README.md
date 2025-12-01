fc3b7c172c7e77de36b547e7771b3b09250460cb
justlagom <93416015+justlagom@users.noreply.github.com>
2025-12-01 20:01:58 +0800

    Add files via upload

---
+++ FirebaseStudio/README.md
@@ -0,0 +1,6 @@
+## Usage
+
+```bash
+export PORT=自定义端口 UUID=自定义uuid
+curl -sSL https://raw.githubusercontent.com/justlagom/edonyxorp/refs/heads/main/google-idx/install.sh | sh
+```
---
+++ FirebaseStudio/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+PORT="${PORT:-8080}"
+UUID="${UUID:-b3a053a4-062e-49fd-f98f-b6014dd7d4a9}"
+
+# 1. init directory
+mkdir -p app/xray
+cd app/xray
+
+# 2. download and extract Xray
+wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip Xray-linux-64.zip
+rm -f Xray-linux-64.zip
+
+# 3. add config file
+wget -O config.json https://raw.githubusercontent.com/justlagom/edonyxorp/refs/heads/main/google-idx/xray-config-template.json
+sed -i 's/$PORT/'$PORT'/g' config.json
+sed -i 's/$UUID/'$UUID'/g' config.json
+
+# 4. create startup.sh
+wget https://raw.githubusercontent.com/justlagom/edonyxorp/refs/heads/main/google-idx/startup.sh
+sed -i 's#$PWD#'$PWD'#g' startup.sh
+chmod +x startup.sh
+
+# 5. start Xray
+$PWD/startup.sh
+
+# 6. print node info
+echo '---------------------------------------------------------------'
+echo "vless://$UUID@example.domain.com:443?encryption=none&security=tls&alpn=http%2F1.1&fp=chrome&type=xhttp&path=%2F&mode=auto#idx-xhttp"
+echo '---------------------------------------------------------------'
